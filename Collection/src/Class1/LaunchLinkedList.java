bee4a5e1ffc343976b8c9fd6f1540d2a7360c4df
safi-sfn <mdsafiruddin24@gmail.com>
2025-06-25 20:01:18 +0530

    feat(collections): demonstrate LinkedList operations with mixed types
    - Create LinkedList and add heterogeneous elements (Integer, Double,
    String, null)
    - Showcase key LinkedList methods:
      * addFirst(), offer(), offerFirst()
      * peekFirst(), peekLast()
      * pollFirst(), pollLast()
    - Print list state after each operation
    
    Note: Uses raw type LinkedList for demonstration purposes

--- Collection/src/Class1/LaunchLinkedList.java
+++ Collection/src/Class1/LaunchLinkedList.java
@@ -1,8 +1,31 @@
 package Class1;
 
+import java.util.LinkedList;
+
 public class LaunchLinkedList {
 
 	public static void main(String[] args) {
+		
+		LinkedList lList = new LinkedList();
+		lList.add(24);
+		lList.add(20);
+		lList.add(56);
+		lList.add(93);
+		lList.add(48.90);
+		System.out.println(lList);
+		
+		lList.addFirst(50);
+		lList.add(null);
+		System.out.println(lList);
+		
+		lList.offer("java");
+		lList.offerFirst("SpringBoot");
+		System.out.println(lList);
+		
+		System.out.println(lList.peekFirst()+" , "+lList.peekLast());
+		System.out.println(lList);
+		System.out.println(lList.pollFirst()+" , "+lList.pollLast());
+		System.out.println(lList);
 		
 
 	}
