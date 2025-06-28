8889f7fbeeaf1b58279a1f5fd2d11ab072b930bd
safi-sfn <mdsafiruddin24@gmail.com>
2025-06-28 10:10:02 +0530

    feat(collections): showcase PriorityQueue's heap behavior
    - Demonstrate default min-heap implementation:
      * Elements auto-sorted via Comparable (Integer)
      * Head always contains minimum value
    - Insert 8 integers in arbitrary order
    - Verify ordering via toString() output
    
    Note: Uses raw type for simplicity; production code should use generics

---
+++ Collection/src/Class1/LaunchPriorityQueue.java
@@ -0,0 +1,25 @@
+package Class1;
+
+import java.util.PriorityQueue;
+
+public class LaunchPriorityQueue {
+
+	public static void main(String[] args) {
+		
+		PriorityQueue pq = new PriorityQueue();
+		
+		pq.add(67);
+		pq.add(132);
+		pq.add(167);
+		pq.add(47);
+		pq.add(90);
+		pq.add(65);
+		pq.add(123);
+		pq.add(48);
+		
+		
+		System.out.println(pq);
+
+	}
+
+}
