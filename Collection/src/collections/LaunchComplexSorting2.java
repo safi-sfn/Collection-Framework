93366b702dc2d68898c52133ef0b1b0d8bbae0fa
safi-sfn <mdsafiruddin24@gmail.com>
2025-06-30 15:55:32 +0530

    feat(collections): implement lambda-based comparators for TestCricket
    - Create TestCricket POJO with name and score attributes
    - Demonstrate two lambda comparator approaches:
      1. Name-based sorting (case-insensitive)
      2. Score-based sorting (numeric comparison)
    - Show Collections.sort() in action with custom comparators

---
+++ Collection/src/collections/LaunchComplexSorting2.java
@@ -0,0 +1,62 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+class TestCricket {
+	
+	String name;
+	int score;
+	
+	public TestCricket(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	@Override
+	public String toString() {
+		return "TestCricketer [name=" + name + ", score=" + score + "]";
+	}
+	
+	
+}
+
+public class LaunchComplexSorting2 {
+
+	public static void main(String[] args) {
+		
+//		TestCricket tcr = new TestCricket();
+		ArrayList<TestCricket> arTc = new ArrayList<>();
+		arTc.add(new TestCricket("Rohit",9000));
+		arTc.add(new TestCricket("Virat",10000));
+		arTc.add(new TestCricket("Sachin",15000));
+		arTc.add(new TestCricket("Dhoni",7000));
+		
+		System.out.println(arTc);
+	// Sort by name with the help of lambda
+		Comparator<TestCricket> comp = (p1,p2) -> p1.getName().compareToIgnoreCase(p2.getName());
+//		Collections.sort(arTc,comp);
+//		System.out.println(arTc);
+	
+	// sort by score with the help of lambda
+		Comparator<TestCricket> comp2 = (p1,p2) -> {
+			if( p1.getScore()>p2.getScore()) 
+				return 1;
+			else
+				return -1;
+					};				
+		Collections.sort(arTc,comp2);
+		System.out.println(arTc);
+
+	}
+
+}
