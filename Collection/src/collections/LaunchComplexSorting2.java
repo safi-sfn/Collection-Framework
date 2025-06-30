30442415fc2265862217fb5c222a71ac59c03839
safi-sfn <mdsafiruddin24@gmail.com>
2025-06-30 16:35:32 +0530

    feat(collections): implement natural ordering with Comparable interface
    - Modified TestCricket to implement Comparable<TestCricket>

--- Collection/src/collections/LaunchComplexSorting2.java
+++ Collection/src/collections/LaunchComplexSorting2.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-class TestCricket {
+class TestCricket implements Comparable<TestCricket> {
 	
 	String name;
 	int score;
@@ -26,6 +26,17 @@ class TestCricket {
 	public String toString() {
 		return "TestCricketer [name=" + name + ", score=" + score + "]";
 	}
+
+	
+	// Comparable's method
+	@Override
+	public int compareTo(TestCricket o) {
+				
+		if(this.score > o.score)
+			return 1;
+		else
+			return -1;
+	}
 	
 	
 }
@@ -41,21 +52,27 @@ public class LaunchComplexSorting2 {
 		arTc.add(new TestCricket("Sachin",15000));
 		arTc.add(new TestCricket("Dhoni",7000));
 		
-		System.out.println(arTc);
+//		System.out.println(arTc);
 	// Sort by name with the help of lambda
-		Comparator<TestCricket> comp = (p1,p2) -> p1.getName().compareToIgnoreCase(p2.getName());
+//		Comparator<TestCricket> comp = (p1,p2) -> p1.getName().compareToIgnoreCase(p2.getName());
 //		Collections.sort(arTc,comp);
 //		System.out.println(arTc);
 	
 	// sort by score with the help of lambda
-		Comparator<TestCricket> comp2 = (p1,p2) -> {
-			if( p1.getScore()>p2.getScore()) 
-				return 1;
-			else
-				return -1;
-					};				
-		Collections.sort(arTc,comp2);
+//		Comparator<TestCricket> comp2 = (p1,p2) -> {
+//			if( p1.getScore()>p2.getScore()) 
+//				return 1;
+//			else
+//				return -1;
+//					};				
+//		Collections.sort(arTc,comp2);
+		
+	
+	// After Comparable 
 		System.out.println(arTc);
+		Collections.sort(arTc);
+		System.out.println(arTc);
+		
 
 	}
 
